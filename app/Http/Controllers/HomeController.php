94002adeaab095ce32bd06a289fe826c49d4b7a9
Anthony Navarro <anthonarro@gmail.com>
2020-09-10 18:02:08 -0700

    Add Home controller;

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Recipe;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $latest_recipes = Recipe::where('is_public', 1)
+                                    ->orderBy('created_at', 'desc')
+                                    ->take(5)
+                                    ->get();
+
+        dd($latest_recipes);
+
+        $data = compact('latest_recipes');
+        return view('home', $data);
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Cookbook</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('Welcome to Cookbook!') }}
-                </div>
-            </div>
+            @include('layouts.partials.latest-recipes')
         </div>
     </div>
 </div>
